60ba1333ca207e988d4fec64c96531fa0ef9ea32
Sabbir Ahmed <sabbir.ruet.cse@gmail.com>
2023-05-24 12:16:20 +0600

    Learned Interface

--- learning-go/main.go
+++ learning-go/main.go
@@ -62,7 +62,9 @@ package main
 
 // }
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Address struct {
 	street string
@@ -76,13 +78,70 @@ type Person struct {
 	address Address
 }
 
+type myNumber int
+
+func (num myNumber) square() myNumber {
+	if num == 0 {
+		return 1
+	}
+	return num * num
+}
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+// Returning multiple value
+func vals() (int, string) {
+	return 3, "Another variable"
+}
+
+type Animal interface {
+	walk() string
+	bark() string
+}
+
+type Dog struct {
+	w string
+	b string
+}
+
+func (d Dog) walk() string {
+	return d.w
+}
+
+func (d Dog) bark() string {
+	return d.b
+}
+
+type User struct {
+	Name  string
+	Email string
+}
+
+func (u User) userDetails() string {
+	return fmt.Sprintf("User name is :%s and email is: %s", u.Name, u.Email)
+}
+
+func (u *User) changeDetails(newName, newEmail string) {
+	u.Name = newName
+	u.Email = newEmail
+}
+
+// Empty Interface
+type MyString string
+
+type Rectangular struct {
+	width  float64
+	height float64
+}
+
+func emptyInterface(i interface{}) {
+	fmt.Printf("Value given to emptyInterface function is of type '%T' with value %v\n", i, i)
+}
+
 func main() {
-	defer fmt.Println("Good bye! 1")
-	defer fmt.Println("Good bye! 2")
-	defer fmt.Println("Good bye! 3")
-	defer fmt.Println("Good bye! 4")
-	defer fmt.Println("Good bye! 5")
-	defer fmt.Println("Good bye! 6")
+	defer fmt.Println("Good bye! with defer")
 	fmt.Println("Good bye! without defer")
 	var_1, var_2 := 1, "hi"
 	//declaring for the first time
@@ -131,13 +190,31 @@ func main() {
 
 	fmt.Println(person)
 
-}
+	num := myNumber(25)
+	sq := num.square()
+	fmt.Printf("The square of %d is %d \n", num, sq)
+	fmt.Println("")
+	fmt.Println("")
 
-func plus(a int, b int) int {
-	return a + b
-}
+	var a Animal = Dog{"Dog is walking.....!!!", "Dog is barking....!!!"}
+	fmt.Println("!.....Dog......!")
+	fmt.Println(a.walk())
+	fmt.Println(a.bark())
 
-// Returning multiple value
-func vals() (int, string) {
-	return 3, "Another variable"
+	fmt.Println("")
+	fmt.Println("")
+
+	user1 := User{Email: "John Doe", Name: "johndoe@Golinuxcloud.com"}
+	fmt.Println(user1.userDetails())
+	user1.changeDetails("Mary Doe", "marydoe@Golinuxcloud.com")
+	fmt.Println(user1.userDetails())
+	fmt.Println("")
+	fmt.Println("")
+
+	myString := MyString("My name is MD ABU SALMAN HOSSAIN")
+	rectangular := Rectangular{5.2, 7}
+
+	emptyInterface(myString)
+	emptyInterface(rectangular)
+
 }
